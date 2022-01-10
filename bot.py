8235dc93cb10fbc3b56072eff0bc2c4854c1d1f2
zoey <optionaltoast420@gmail.com>
2022-01-10 21:18:33 +0000

    started

--- bot.py
+++ bot.py
@@ -1,5 +1,21 @@
-def main():
-    
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from random import randint
 
+def main():
+    with open("words") as w:
+        words = w.readlines()
+    
+    #TODO: better first guess algorithm
+    first_guess = words[randint(0, len(words))]
+    print(first_guess)
+
+     
+    driver = webdriver.Safari()
+    driver.implicitly_wait(10)
+    driver.get("https://www.powerlanguage.co.uk/wordle/")
+
+
+    
 if __name__ == "__main__":
     main()
